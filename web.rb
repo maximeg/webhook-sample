9515370686879a2686053ed453628b5b8f147b09
Maxime Garcia <maxime.garcia@dimelo.com>
2014-07-31 11:04:30 +0200

    micro endpoint

--- web.rb
+++ web.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
 
-get '/' do
-  "Hello, world"
+get "/endpoint" do
+  if params["hub.mode"] == "challenge"
+    status 200
+    body params["hub.challenge"].to_s
+  else
+    status 400
+    body ""
+  end
+end
+
+post "/endpoint" do
+  status 200
+  body request.body
 end
