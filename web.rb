d0a7ef744664181ace2eff1ddabc48d54f840fc2
Maxime Garcia <maxime.garcia@dimelo.com>
2014-07-31 11:24:06 +0200

    text

--- web.rb
+++ web.rb
@@ -1,12 +1,19 @@
 require 'sinatra'
 
+get "/" do
+  body <<-HTML
+    <h1>Webhook consumer sample</h1>
+    <p><a href="/endpoint">/endpoint</a></p>
+  HTML
+end
+
 get "/endpoint" do
   if params["hub.mode"] == "challenge"
     status 200
     body params["hub.challenge"].to_s
   else
     status 400
-    body ""
+    body "Invalid challenge request"
   end
 end
 
