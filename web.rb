26b9e214f5a96c8323779986d676e122eb13e05d
Maxime Garcia <maxime.garcia@dimelo.com>
2014-07-31 12:38:49 +0200

    post

--- web.rb
+++ web.rb
@@ -1,6 +1,7 @@
 $stdout.sync = true
 
 require 'sinatra'
+require 'json'
 
 get "/" do
   body <<-HTML
@@ -22,5 +23,22 @@ end
 
 post "/endpoint" do
   puts "WEBHOOK: Body received: #{request.body.string}"
-  status 200
+  payload = JSON.load(request.body.string)
+  if events = payload["events"]
+    events.all? do |event|
+      if event["type"]
+        puts "WEBHOOK: Event received: #{event["type"]}: #{event.inspect}"
+      else
+        puts "WEBHOOK: Invalid Event received: #{event.inspect}"
+        status 400
+        body "invalid event"
+      end
+    end
+
+    status 200
+  else
+    puts "WEBHOOK: No event received"
+    status 400
+    body "no events"
+  end
 end
