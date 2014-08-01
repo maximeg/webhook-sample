432c47021db27436afec1c59ffb342fe0b1d7dd6
Maxime Garcia <maxime.garcia@dimelo.com>
2014-08-01 10:24:19 +0200

    name

--- web.rb
+++ web.rb
@@ -6,12 +6,13 @@ require 'json'
 get "/" do
   body <<-HTML
     <h1>Webhook consumer sample</h1>
-    <p><a href="/endpoint">/endpoint</a></p>
+    <p>Use a name as a namespace</p>
+    <p><a href="/my-name/endpoint">/:name/endpoint</a></p>
   HTML
 end
 
-get "/endpoint" do
-  puts "WEBHOOK: Params received: #{params.inspect}"
+get "/:name/endpoint" do
+  puts "[WEBHOOK #{params[:name]}] Params received: #{params.inspect}"
   if params["hub.mode"] == "subscribe" && params["hub.challenge"] && params["hub.verify_token"]
     status 200
     body params["hub.challenge"].to_s
@@ -21,15 +22,15 @@ get "/endpoint" do
   end
 end
 
-post "/endpoint" do
-  puts "WEBHOOK: Body received: #{request.body.string}"
+post "/:name/endpoint" do
+  puts "[WEBHOOK #{params[:name]}] Body received: #{request.body.string}"
   payload = JSON.load(request.body.string)
   if events = payload["events"]
     events.all? do |event|
       if event["type"]
-        puts "WEBHOOK: Event received: #{event["type"]}: #{event.inspect}"
+        puts "[WEBHOOK #{params[:name]}] Event received: #{event["type"]}: #{event.inspect}"
       else
-        puts "WEBHOOK: Invalid Event received: #{event.inspect}"
+        puts "[WEBHOOK #{params[:name]}] Invalid Event received: #{event.inspect}"
         status 400
         body "invalid event"
       end
@@ -37,7 +38,7 @@ post "/endpoint" do
 
     status 200
   else
-    puts "WEBHOOK: No event received"
+    puts "[WEBHOOK #{params[:name]}] No event received"
     status 400
     body "no events"
   end
